5073de05493e65b0e12cb23e4069f4082efa34f6
Anas <anaska1311@gmail.com>
2026-06-22 22:14:15 +0800

    Removed JSON header in fetch products

--- src/api/fetch_products.php
+++ src/api/fetch_products.php
@@ -1,11 +1,8 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
 
 require_once __DIR__ . '/../config/database.php';
-
-$category = isset($_GET['search']) ? trim($_GET['search']) : '';
 
 function fetchProducts($category): array
 {
