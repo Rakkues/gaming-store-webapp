fea8d24db2c4bcc2544be5de3a7363dd03932ed3
Qaid-Danial <muhammadqaidd@gmail.com>
2026-06-24 09:41:24 +0800

    FIx: login.php & register.php uses database helper

--- public/pages/auth/register.php
+++ public/pages/auth/register.php
@@ -2,17 +2,9 @@
 
 session_start();
 
-$DB_HOST = "localhost";
-$DB_NAME = "gaming_retail_store";
-$DB_USER = "root";
-$DB_PASS = "";
+require_once __DIR__ . '/../../../src/config/database.php';
 
-$pdo = new PDO(
-    "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
-    $DB_USER,
-    $DB_PASS,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false]
-);
+$pdo = getDBConnection();
 
 if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
