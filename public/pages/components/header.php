ebb632d20d32da2d11b2eaadc39c544313a32ebe
Qaid-Danial <muhammadqaidd@gmail.com>
2026-06-27 00:05:49 +0800

    Fix: Error code in product page due to isLoggedIn state not readable :/

--- public/pages/components/header.php
+++ public/pages/components/header.php
@@ -1,3 +1,10 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$isLoggedIn = !empty($_SESSION['logged_in']);
+?>
 <header>
   <div class="header-top">
     <div class="store-logo-container">
